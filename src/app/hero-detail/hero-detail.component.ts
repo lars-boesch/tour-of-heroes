f5d082e23961bd261d5e7bbdeb936a58759f9d8d
Lars Bösch <lboesch@kolax.ergon.ch>
2022-06-23 11:25:48 +0200

    final tour of heroes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -29,7 +29,10 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
-  goBack(): void {
-    this.location.back();
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.location.back());
+    }
   }
 }
